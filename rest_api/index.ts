a5b6d3d62528cabcead0e3d20a6d8ccb4fc70d05
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-03-15 22:24:13 +0700

    Add Easter Egg response to root route

--- rest_api/index.ts
+++ rest_api/index.ts
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/v1", apiRoutes);
+app.use("/", (req, res) => {
+  // Send an Easter Egg!
+  res.send("Shh... You weren't supposed to find this.")
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
