73ceab201472e6e0187466538d22ba096a3180b4
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-03-15 18:35:41 +0700

    Refactor helloHandler to use default export and update import in apiRoutes

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
 
-export const helloHandler = (req: Request, res: Response) => {
+export default function helloHandler(req: Request, res: Response) {
   res.json({ message: "Hello, world!" });
-};
+}
