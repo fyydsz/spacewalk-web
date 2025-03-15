446173b1fb543240ec5c6785c876568411a5e61d
Fyy <121779725+fyydsz@users.noreply.github.com>
2025-03-16 04:38:43 +0700

    Disable ETag generation and set Cache-Control headers for API responses

--- rest_api/index.ts
+++ rest_api/index.ts
@@ -7,6 +7,7 @@ configDotenv();
 const port = process.env.PORT;
 
 const app = express();
+app.disable('etag')
 
 app.use(cors());
 app.use(express.json());
--- rest_api/v1/hello.ts
+++ rest_api/v1/hello.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
 export default function helloHandler(req: Request, res: Response) {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
   res.json({ message: "Hello, world!" });
 }
